c4a718729bd7062d33f55184bd2b0e09a844675f
vpzed <30138020+vpzed@users.noreply.github.com>
2017-11-20 02:20:04 -0600

    further work on stat UI and menus

--- src/components/Wookieville.vue
+++ src/components/Wookieville.vue
@@ -5,15 +5,23 @@
         <div class="mt-3">
           <b-alert show variant="primary" align="center" class="font-weight-bold">Wookieville Clan Leaderboards</b-alert>
         </div>
+        <div class="mt-3">
+          <p>Select Activity Mode</p>
+          <b-form-select v-model="activityModeSelected" :options="activityModeOptions"></b-form-select>
+        </div>
+        <div class="mt-3">
+          <p>Select Statistic</p>
+          <b-form-select v-model="statIdSelected" :options="statIdOptions"></b-form-select>
+        </div>
       </b-col>
     </b-row>
     <b-row align-h="center">
       <b-col cols="8">
         <div class="mt-3">
-          <b-button variant="danger" type="dark" v-on:click="FETCH_ACCT_STATS">Test</b-button>
+          <b-button variant="success" type="dark" v-on:click="fetchAcctStats(selectedParams)">Get Leaderboard</b-button>
         </div>
         <div class="mt-3 mb-5">
-          <b-table striped :items="acctStats"></b-table>
+          <b-table :items="acctStats" striped></b-table>
         </div>
       </b-col>
     </b-row>
@@ -21,22 +29,46 @@
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex'
+import { acctStatMenus } from '@/store/testMenus'
+
 export default {
   name: 'Wookieville',
-  computed: mapGetters([
-    'acctStats'
-  ]),
-  methods: mapActions([
-    'FETCH_ACCT_STATS'
-  ]),
+  computed: {
+    acctStats () {
+      return this.$store.getters.acctStats
+    },
+    selectedParams () {
+      return {
+        allianceId: 2,
+        activityMode: this.activityModeSelected,
+        statId: this.statIdSelected
+      }
+    }
+  },
+  methods: {
+    fetchAcctStats (params) {
+      this.$store.dispatch('FETCH_ACCT_STATS', params)
+    }
+  },
   data: function () {
     return {
-      params: null,
-      tableData: null
+      activityModeSelected: null,
+      activityModeOptions: acctStatMenus.activityModeOptions,
+      statIdSelected: null,
+      statIdOptions: acctStatMenus.statIdOptions
     }
   }
 }
+
+/*
+  Account Stats query variables:
+  select=display_name,basic_display_value,activity_mode,stat_id
+  alliance_id=eq.2
+  activity_mode=eq.trialsofthenine
+  stat_id=eq.winLossRatio
+  order=basic_value.desc
+  limit=10
+*/
 </script>
 
 <style>
--- src/store/index.js
+++ src/store/index.js
@@ -18,8 +18,8 @@ const mutations = {
 
 const actions = {
   async FETCH_ACCT_STATS ({ commit }, payload) {
-    // de-reference params and build url
-    const url = 'http://192.168.1.4:3000/mv_account_stats?select=display_name,basic_display_value,activity_mode,stat_id&alliance_id=eq.2&activity_mode=eq.trialsofthenine&stat_id=eq.winLossRatio&order=basic_value.desc&limit=10'
+    const url = `http://192.168.1.4:3000/mv_account_stats?select=display_name,basic_display_value&alliance_id=eq.${payload.allianceId}&activity_mode=eq.${payload.activityMode}&stat_id=eq.${payload.statId}&order=basic_value.desc&limit=10`
+    // const url = `http://192.168.1.4:3000/mv_account_stats?select=display_name,basic_display_value,activity_mode,stat_id&alliance_id=eq.${payload.allianceId}&activity_mode=eq.${payload.activityMode}&stat_id=eq.${payload.statId}&order=basic_value.desc&limit=10`
     const apiResponse = await axios.get(url)
     commit('RECEIVE_ACCT_STATS', { acctStats: apiResponse.data })
   }
